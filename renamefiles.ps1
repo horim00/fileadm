a62a079a92cc2c0f79e298b592a9aac8869a26d2
horim00 <mho@pobox.com>
2024-09-22 09:46:20 +0900

    Update renamefiles.ps1
    開始番号指定
    パラメタ -listfileを-listに変更
    ファイル名を保存して番号付けする機能としてDirective 'asis'を追加

--- renamefiles.ps1
+++ renamefiles.ps1
@@ -3,12 +3,14 @@ using namespace System.Collections.Generic
 #parameter
 param(# Parameter help description
 [Parameter(Mandatory=$true)]
-    [string] $listfile,
+    [string] $list,
 [Parameter(Mandatory=$false)]
-[Int32] $kubun=0
+[Int32] $kubun=0,
+[Parameter(Mandatory=$false)]
+[Int32] $start=0   # start from 1
 )
-    
-$list = [List[PSCustomObject]]::new()
+
+$objlist = [List[PSCustomObject]]::new()
 #Hash {Directive,how many times the directive occurres}like {書誌,0][提示文献,2][図面,0]]
 $DirectiveHash =@{}
 #count target file , start from 1
@@ -19,12 +21,28 @@ $TargetFileNumHash=@{}
 $TargetFileNumBaseHash=@{}
 $DirectiveOrder=[List[string]]::new()
 
-$my_file = Get-Content $listfile
+$my_file = Get-Content $list
 #read file and store
 Foreach ($my_string  in $my_file) {
     #split into variable 
     $one, $two,$three = $my_string.split(",")
-    $list.Add([pscustomobject]@{FName=$one
+    # if Directive is "asis", use filename trunk as Directive 
+    if ($two -eq "asis")
+    {
+        if ($one -match "^\d\d_(.*)\.pdf$")
+        {
+            $two = $Matches.1
+        }
+        elseif ($one -match "^(.*)\.pdf$")
+        {
+            $two = $Matches.1
+        }
+        else {
+            $two = $one
+        }
+    }
+
+    $objlist.Add([pscustomobject]@{FName=$one
         Directive=$two
         TargetFile=""
         } )
@@ -37,54 +55,63 @@ Foreach ($my_string  in $my_file) {
         $DirectiveHash.Add($two, 0)
         $DirectiveOrder.Add($two)
     }
+
 }
 
 #for each object
-foreach ($obj in $list)
+foreach ($obj in $objlist)
 {
     Write-Host $obj.FName
     Write-Host $obj.Directive
 }
 Write-Host $DirectiveHash
 Write-Host "区分 $kubun"
+Write-Host "開始 $start"
 
-$count = 0
+$count = $start
 $filename=""
-foreach ($obj in $list)
+foreach ($obj in $objlist)
 {
-    if ($kubun -eq 8)
+     if ($obj.FName  -match "_提示文献 \d+_(.*)$")
     {
-        if ($obj.FName  -match "_")
-        {
-            $filename =     ($obj.FName -split "_")[-1]
-        }
-        else
-        {
-            $filename = $obj.FName
-        } 
-    }
+        #pick filename from dd_提示文献n_filename 
+        #        $filename =   ($obj.FName -split "_")[-1]
+        $filename = $Matches.1
+     }
+     elseif ($obj.FName -match "^\d+_(.*)$")
+     {
+        #pick filename \dd_filename
+        $filename = $Matches.1
+     }
+     else
+    {
+        $filename = $obj.FName
+    } 
 
     # if directive appears once, no need to generate directive number string
     if ($DirectiveHash[$obj.Directive] -eq 0)
     {
         $count++
 #        $countString = $count.ToString("00")
-        if (($kubun -eq 8 ) -and  ($obj.Directive  -match "提示文献"))
+            if (($kubun -eq 8 ) -and  ($obj.Directive  -match "提示文献"))
         {
+            #kubun 8 only:  dd_提示文献_filename
             $obj.TargetFile = $count.ToString("00")+ "_" + $obj.Directive+"_"+$filename    
         }
         else {
+            #otherwise dd_提示文献n
             $obj.TargetFile = $count.ToString("00")+ "_" + $obj.Directive+".pdf"
         }
     }
     else {
+        #count up Directive eg, 提示文献1, 提示文献2
         if ($TargetFileNumHash.ContainsKey($obj.Directive))
         {
             #count up target file number for the directive
             $TargetFileNumHash[$obj.Directive]++
         }
         else {
-            #set number for the directive to "1"
+            #set number for the Directive to 1
             $TargetFileNumHash.Add($obj.Directive, 1)
             $count++
             $TargetFileNumBaseHash.Add($obj.Directive, $count)
@@ -93,23 +120,26 @@ foreach ($obj in $list)
         }
         # total number = base + count for the directive
         $totalnum = $TargetFileNumBaseHash[$obj.Directive] + $TargetFileNumHash[$obj.Directive] - 1
+        
         if (($kubun -eq 8 ) -and  ($obj.Directive  -match "提示文献"))
         {
+            #区分８アミューズメント仕様  \d\d_提示文献N_文献番号.pdf
             $obj.TargetFile = $totalnum.ToString("00")  + "_"  + $obj.Directive + $TargetFileNumHash[$obj.Directive].ToString()+"_"+$filename
         }
         else {
+            # \d\d_DirectiveN.pdf
             $obj.TargetFile = $totalnum.ToString("00")  + "_"  + $obj.Directive + $TargetFileNumHash[$obj.Directive].ToString()+".pdf"
         }
     }
 }
 
-foreach ($obj in $list)
+foreach ($obj in $objlist)
 {
-
-    Rename-Item -Path $obj.FName -NewName $obj.TargetFile
-
-    Write-Host "rename  $obj"
-#    Write-Host $obj.FName
- #   Write-Host $obj.Directive
- #   Write-Host $obj.TargetFile
+    try {
+       Move-Item -Path $obj.FName -Destination $obj.TargetFile  -Force
+    }
+    catch {
+        Write-Host "rename file failed"
+        Write-Host $_
+    }
 }
